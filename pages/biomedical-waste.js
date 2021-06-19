fc4b9eba7fe7da1291c05a27bc824b25c9e94579
Sanjoth J Rai <sanjr4279@gmail.com>
2021-06-19 16:13:26 +0530

    intial content

--- pages/biomedical-waste.js
+++ pages/biomedical-waste.js
@@ -10,8 +10,17 @@ export default function BiomedicalWaste() {
                 <h1>
                     Biomedical Waste
                 </h1>
+                <br/><br/>
                 <p>
-                    Content
+                    Any waste which is generated during the diagnosis, treatment or immunization of human beings or animals or in research activities pertaining there to or in the production or testing of biological.
+                </p>
+                <br /><br />
+                <h3>
+                    Disposal
+                </h3>
+                <br />
+                <p>
+                    If you are in home isolation, club the dry waste and sanitary waste together and keep them in yellow (non-chlorinated) bags. These bags would be collected by regular waste collectors/pourakarmikas, who would then hand it over to an authorised vendor who runs a CBWTF (Common Bio-medical Waste Treatment and Disposal Facility).
                 </p>
             </div>
         </Layout>
